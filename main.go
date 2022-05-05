bfae7e6585da53eed28de64d47ebe851f26cfd4c
Ragnar Paide <16119863+ragnarpa@users.noreply.github.com>
2022-05-05 13:02:12 +0300

    Add logging

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	r := gin.Default()
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	r := gin.New()
 	r.GET("/ping", func(c *gin.Context) {
+		logrus.
+			WithFields(
+				logrus.Fields{
+					"agent": c.Request.UserAgent(),
+				},
+			).
+			Info("/ping called")
+
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
